230fb17a75398a19bb1976f3c7034211c0c99b60
Olga Gavrushenko <ogavby@gmail.com>
2022-11-10 16:52:39 -0800

    added function to return licence link

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,18 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'none' || license === 'The Unlicense') {
+    return "";
+  }
+  license = license.replaceAll(' ', '%20');
+  return `![License badge](https://img.shields.io/static/v1?label=license&message=${license}&color=green)`;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
