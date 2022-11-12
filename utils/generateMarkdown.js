7fb6f5304b0db2c110be3e8a1ac4bfe018fbc9dd
Olga Gavrushenko <ogavby@gmail.com>
2022-11-11 22:52:17 -0800

    implemented generate markdown function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,71 +1,128 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license === 'none' || license === 'The Unlicense') {
+  if (license === 'none') {
     return "";
+  } else {
+    license = license.replaceAll(' ', '%20');
+    return `![License badge](https://img.shields.io/static/v1?label=license&message=${license}&color=green)`;
   }
-  license = license.replaceAll(' ', '%20');
-  return `![License badge](https://img.shields.io/static/v1?label=license&message=${license}&color=green)`;
 }
 
-// TODO: Create a function that returns the license link
+// Function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  let license_link;
 
   switch (license) {
     case 'Apache License 2.0': 
-      return_link = "https://choosealicense.com/licenses/apache-2.0/";
+      return "https://choosealicense.com/licenses/apache-2.0/";
       break;
     case 'GNU General Public License v3.0': 
-      return_link = "";
+      return "https://choosealicense.com/licenses/gpl-3.0/";
       break;
     case 'MIT License': 
-      return_link = "";
+      return "https://choosealicense.com/licenses/mit/";
       break;
     case 'BSD 2-Clause “Simplified” License': 
-      return_link = "";
+      return "https://choosealicense.com/licenses/bsd-2-clause/";
       break;
     case 'BSD 3-Clause “New” or “Revised” License': 
-      return_link = "";
+      return "https://choosealicense.com/licenses/bsd-3-clause/";
       break;
     case 'Boost Software License 1.0': 
-      return_link = "";
-      break;
-  case 'Creative Commons Zero v1.0 Universal': 
-      return_link = "";
+      return "https://choosealicense.com/licenses/bsl-1.0/";
       break;
   case 'Eclipse Public License 2.0': 
-      return_link = "";
+      return "https://www.eclipse.org/legal/epl-2.0/";
       break;
   case 'GNU Affero General Public License v3.0': 
-      return_link = "";
+      return "https://choosealicense.com/licenses/agpl-3.0/";
       break;      
   case 'GNU General Public License v2.0': 
-      return_link = "";
+      return "https://choosealicense.com/licenses/gpl-3.0/";
       break;
   case 'GNU Lesser General Public License v2.1': 
-      return_link = "";
+      return "https://choosealicense.com/licenses/lgpl-3.0/";
       break;
   case 'Mozilla Public License 2.0': 
-      return_link = "";
+      return "https://choosealicense.com/licenses/mpl-2.0/";
       break;
+  case 'The Unlicense': 
+      return "https://choosealicense.com/licenses/unlicense/";
+      break;      
   default: 
-      return_link = "";
+      return "";
   }
-
-  return license_link;
 }
 
-// TODO: Create a function that returns the license section of README
+// Function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let currentLicenseLink = renderLicenseLink(license);
+  if (license !== "none") {
+    return `This application is available under the license: ${license}. 
+    See the LICENSE file for more info. Full details available by link ${currentLicenseLink}. 
+    `
+  } else {
+    return `n/a`
+  }
+}
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
 
-`;
+let currentLicenseBadge = renderLicenseBadge(data.license);
+let currentLicenseSection = renderLicenseSection(data.license);
+
+return `
+  # ${data.project}
+
+  ${currentLicenseBadge}
+
+  ## Description
+  
+  ${data.description}
+
+  ![this image show the shcreenshot of the application](${data.screenshot})
+  
+  ## Table of Contents
+ 
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  - [Questions](#questions)
+  - [Tests](#tests)
+  
+  ## Installation
+  
+  ${data.installation}
+  
+  ## Usage
+  
+  ${data.usage}
+  
+  ## Credits
+  
+  ${data.credits}
+  
+  ## License
+  
+  ${currentLicenseBadge}
+
+  ${currentLicenseSection}
+ 
+  ## Questions
+  
+  Link to my GitHub profile https://github.com/${data.username}/
+  
+  If you have additional questions or proposals please email me: [${data.email}](mailto:${data.email})
+    
+  ## Tests
+  
+  ${data.tests}
+  `;
 }
 
 module.exports = generateMarkdown;
